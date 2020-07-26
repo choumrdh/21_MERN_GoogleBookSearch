ba706c2698d7d418306e8c367300da4d86b25cb5
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-26 16:25:36 -0700

    added seed file

--- models/book.js
+++ models/book.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description:{ type: String, trim: true},
-  image: String,
-  link:{type: String, trim: true},
-  date: { type: Date, default: Date.now }
+  description: { type: String, trim: true },
+  image: { type: String, trim: true },
+  link: { type: String, trim: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Book = mongoose.model("Book", bookSchema);
