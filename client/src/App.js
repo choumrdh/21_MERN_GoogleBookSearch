7c0041e4e7dc950ee746ac97ee957889e4d49c17
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-25 18:22:42 -0700

    added possible components folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import Books from "./pages/Books";
-import Nav from "./components/Nav";
+import Header from "./components/header/Header"
+import Navbar from "./components/navbar/Navbar";
+import ResultContainer from "./components/resultcontainer/ResultContainer";
+import Mainpage from "./pages/Mainpage"
+import Savepage from "./pages/Savepage"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Nav />
-      <Books />
+      <Navbar />
+        <Header />
+        <ResultContainer />
+      <Router>
+        
+
+        <Switch>
+          <Route exact path="/" component={Mainpage}/>
+          <Route exact path="/save" component={Savepage}/>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
