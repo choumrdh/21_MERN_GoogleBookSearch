c1ce6e47e575e1f191fda057a322d0217e174065
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-26 16:21:28 -0700

    updated book model

--- models/book.js
+++ models/book.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
+  description:{ type: String, trim: true},
+  image: String,
+  link:{type: String, trim: true},
   date: { type: Date, default: Date.now }
 });
 
