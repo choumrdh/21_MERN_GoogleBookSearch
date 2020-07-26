55c90b5a21d380fd9263459b5ddfbe8684804367
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-25 17:58:27 -0700

    basic code

---
+++ routes/api/books.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// router.get("", ()=>{}) short cut
+// router.post("", ()=>{})
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
